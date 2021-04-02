14e2ef40deb9fc87415aa9712fa7eb42ed0828dc
anton96vice <65628050+anton96vice@users.noreply.github.com>
2021-04-02 09:55:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,92 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import yfinance as yf
 import streamlit as st
+import datetime 
+import talib 
+import ta
+import pandas as pd
+import requests
+yf.pdr_override()
 
-"""
-# Welcome to Streamlit!
+st.write("""
+# Technical Analysis Web Application
+Shown below are the **Moving Average Crossovers**, **Bollinger Bands**, **MACD's**, **Commodity Channel Indexes**, and **Relative Strength Indexes** of any stock!
+""")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.sidebar.header('User Input Parameters')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+today = datetime.date.today()
+def user_input_features():
+    ticker = st.sidebar.text_input("Ticker", 'AAPL')
+    start_date = st.sidebar.text_input("Start Date", '2019-01-01')
+    end_date = st.sidebar.text_input("End Date", f'{today}')
+    return ticker, start_date, end_date
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+symbol, start, end = user_input_features()
 
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+    result = requests.get(url).json()
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+company_name = get_symbol(symbol.upper())
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+start = pd.to_datetime(start)
+end = pd.to_datetime(end)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Read data 
+data = yf.download(symbol,start,end)
 
-    points_per_turn = total_points / num_turns
+# Adjusted Close Price
+st.header(f"Adjusted Close Price\n {company_name}")
+st.line_chart(data['Adj Close'])
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# ## SMA and EMA
+#Simple Moving Average
+data['SMA'] = talib.SMA(data['Adj Close'], timeperiod = 20)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Exponential Moving Average
+data['EMA'] = talib.EMA(data['Adj Close'], timeperiod = 20)
+
+# Plot
+st.header(f"Simple Moving Average vs. Exponential Moving Average\n {company_name}")
+st.line_chart(data[['Adj Close','SMA','EMA']])
+
+# Bollinger Bands
+data['upper_band'], data['middle_band'], data['lower_band'] = talib.BBANDS(data['Adj Close'], timeperiod =20)
+
+# Plot
+st.header(f"Bollinger Bands\n {company_name}")
+st.line_chart(data[['Adj Close','upper_band','middle_band','lower_band']])
+
+# ## MACD (Moving Average Convergence Divergence)
+# MACD
+data['macd'], data['macdsignal'], data['macdhist'] = talib.MACD(data['Adj Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+
+# Plot
+st.header(f"Moving Average Convergence Divergence\n {company_name}")
+st.line_chart(data[['macd','macdsignal']])
+
+## CCI (Commodity Channel Index)
+# CCI
+cci = ta.trend.cci(data['High'], data['Low'], data['Close'], n=31, c=0.015)
+
+# Plot
+st.header(f"Commodity Channel Index\n {company_name}")
+st.line_chart(cci)
+
+# ## RSI (Relative Strength Index)
+# RSI
+data['RSI'] = talib.RSI(data['Adj Close'], timeperiod=14)
+
+# Plot
+st.header(f"Relative Strength Index\n {company_name}")
+st.line_chart(data['RSI'])
+
+# ## OBV (On Balance Volume)
+# OBV
+data['OBV'] = talib.OBV(data['Adj Close'], data['Volume'])/10**6
+
+# Plot
+st.header(f"On Balance Volume\n {company_name}")
+st.line_chart(data['OBV'])
